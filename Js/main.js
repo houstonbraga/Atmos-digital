ebc86043edaad74051c91597b268ab5a72105405
Houston Braga <houstonliverboy123@gmail.com>
2024-06-10 00:31:48 -0300

    adicioanamento de formulario teste 1

--- Js/main.js
+++ Js/main.js
@@ -1,21 +1,4 @@
 //slider para projetos realizados
 
-let currentIndex = 0;
 
-document.querySelector('.slider::after').addEventListener('click', () => {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.projeto').length;
-    if (currentIndex < totalSlides - 1) {
-        currentIndex++;
-        slides.style.transform = `translateX(-${currentIndex * 400}px)`;
-    }
-});
-
-document.querySelector('.slider::before').addEventListener('click', () => {
-    const slides = document.querySelector('.slides');
-    if (currentIndex > 0) {
-        currentIndex--;
-        slides.style.transform = `translateX(-${currentIndex * 400}px)`;
-    }
-});
 
