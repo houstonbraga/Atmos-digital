1c3f9cf1981f6723785d85f4b10c8cc501530c44
Houston Braga <houstonliverboy123@gmail.com>
2024-09-15 01:47:09 -0300

    ajuste de link para projetos realizados

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,19 @@
-//MENU BARS
+//manipulacao de mouse enter nos projetos landing-pages e sistemas
 
+const refMouseEnter = document.querySelectorAll('.projeto-img');
+const linkProject = document.querySelectorAll('.icon-link');
+
+refMouseEnter.forEach((ref, index) => {
+    ref.addEventListener('mouseenter', function() {
+        linkProject[index].classList.remove('hidden');
+        linkProject[index].classList.add('visible');
+    })
+
+    ref.addEventListener('mouseleave', function() {
+        linkProject[index].classList.remove('visible');
+        linkProject[index].classList.add('hidden');
+    })
+})
 
 
 
