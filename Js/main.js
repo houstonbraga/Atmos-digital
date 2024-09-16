82934c129f05a11759e76c7243a80c86f6685706
Houston Braga <houstonliverboy123@gmail.com>
2024-09-16 11:50:30 -0300

    ajuste de link para landing pages terminado, faltando apenas a linkagem

--- Js/main.js
+++ Js/main.js
@@ -7,13 +7,13 @@ refMouseEnter.forEach((ref, index) => {
     ref.addEventListener('mouseenter', function() {
         linkProject[index].classList.remove('hidden');
         linkProject[index].classList.add('visible');
-    })
+    });
 
     ref.addEventListener('mouseleave', function() {
         linkProject[index].classList.remove('visible');
         linkProject[index].classList.add('hidden');
-    })
-})
+    });
+});
 
 
 
